9cc3a9685860b3076ffd58b9d445388973d68bff
Aluno CA <kaioromao12@gmail.com>
2023-11-06 22:22:39 -0300

    aula 06/11

---
+++ aula03/meu-primeiro-projeto/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Contador from './components/Contador';
+
+export default function App() {
+  return (
+    <View>
+    <Contador />
+    </View>
+  );
+}
